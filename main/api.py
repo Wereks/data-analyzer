4a8877ab9c042f60b019279341932d98fd44d5fb
wereks <mateusz.zdon@student.uj.edu.pl>
2021-04-22 21:55:34 +0200

    Restored getting data from external API in class DB

--- main/api.py
+++ main/api.py
@@ -11,16 +11,12 @@ class DB:
 
     def users(self, refresh=False):
         if refresh or self._users is None:
-            #self._users = requests.get("https://jsonplaceholder.typicode.com/users").json()
-            with open('.\\main\\users.json') as f:
-                self._users = json.load(f)
+            self._users = requests.get("https://jsonplaceholder.typicode.com/users").json()
 
         return deepcopy(self._users)
 
     def posts(self, refresh=False):
         if refresh or self._posts is None:
-            with open('.\\main\\posts.json') as f:
-                self._posts = json.load(f)
-            #self._posts = requests.get("https://jsonplaceholder.typicode.com/posts").json()
+            self._posts = requests.get("https://jsonplaceholder.typicode.com/posts").json()
 
         return deepcopy(self._posts)    
