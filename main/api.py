a5aa291fa34563a2a5ce3d262adba692a3636d31
wereks <mateusz.zdon@student.uj.edu.pl>
2021-04-22 23:53:49 +0200

    Deleted import, requests are now send with custom header

--- main/api.py
+++ main/api.py
@@ -1,22 +1,24 @@
 from copy import deepcopy
-import json
 
 import requests
 
 class DB:
     def __init__(self):
-        self.headers = {'User-Agent': 'bot-github.com/Wereks/'}
+        self.headers = {'User-Agent': 'bot-github.com/Wereks/data-analyzer'}
         self._users = None
         self._posts = None
 
+    def _get(self, url):
+        return requests.get(url, headers=self.headers).json()
+
     def users(self, refresh=False):
         if refresh or self._users is None:
-            self._users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+            self._users = self._get("https://jsonplaceholder.typicode.com/users")
 
         return deepcopy(self._users)
 
     def posts(self, refresh=False):
         if refresh or self._posts is None:
-            self._posts = requests.get("https://jsonplaceholder.typicode.com/posts").json()
+            self._posts = self._get("https://jsonplaceholder.typicode.com/posts")
 
         return deepcopy(self._posts)    
